5a764dc81c4f4a877cbc3c0d55a861c68b131eaa
Manish <mjpatel84@gmail.com>
2023-10-21 14:57:33 -0400

    adds mobile styles

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ menuToggleBtn.addEventListener("click", function () {
     navMenu.classList.remove("active");
     navMenu.setAttribute("aria-expanded", false);
     menuToggleBtn.style.display = "none";
-    document.body.style.overflowY = "unset";
+    // document.body.style.overflowY = "unset";
   } else {
     navMenu.classList.add("active");
     navMenu.setAttribute("aria-expanded", true);
     menuToggleBtn.style.display = "block";
-    document.body.style.overflowY = "hidden";
+    // document.body.style.overflowY = "hidden";
   }
 });
 
