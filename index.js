a45dfa69f1fe956ce93fd6f3871a2c9aed9420da
Manish <mjpatel84@gmail.com>
2023-10-22 07:48:42 -0400

    adds dark mode logic

--- index.js
+++ index.js
@@ -19,3 +19,34 @@ menuToggleBtn.addEventListener("click", function () {
 closeMenuBtn.addEventListener("click", function () {
   navMenu.classList.remove("active");
 });
+
+const darkModeToggleBtn = document.getElementById("dark-mode-toggle-btn");
+const label = document.querySelector("label");
+const moon = darkModeToggleBtn.querySelector(".moon");
+const sun = darkModeToggleBtn.querySelector(".sun");
+const logo = document.querySelector(".logo");
+const smallHeading = document.querySelector(".small-heading");
+const h1 = document.querySelector("h1");
+const benefits = document.querySelectorAll(".benefits span");
+const aboutLink = document.querySelector(".about-link");
+
+moon.addEventListener("click", toggleDarkMode);
+sun.addEventListener("click", toggleDarkMode);
+
+function toggleDarkMode() {
+  if (document.body.classList.contains("dark-mode")) {
+    document.body.classList.remove("dark-mode");
+    logo.setAttribute("src", "./assets/alarado-icon-homepage.svg");
+    smallHeading.style.color = "#223344";
+    h1.style.color = "#223344";
+    aboutLink.style.color = "#111729";
+    benefits.forEach((benefit) => (benefit.style.color = "#223344"));
+  } else {
+    document.body.classList.add("dark-mode");
+    logo.setAttribute("src", "./assets/alarado-icon-homepage-dark-mode.svg");
+    smallHeading.style.color = "#FFFFFF";
+    h1.style.color = "#FFFFFF";
+    aboutLink.style.color = "#FFFFFF";
+    benefits.forEach((benefit) => (benefit.style.color = "#909193"));
+  }
+}
